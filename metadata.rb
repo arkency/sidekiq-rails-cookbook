220f47a2ae4733fb0aaf50a8092790caa1026fea
Robert Pankowecki <robert.pankowecki@gmail.com>
2014-02-17 12:14:16 +0100

    Depends on runit cookbook

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'Apache 2.0'
 description      'Sidkiq worker starting in rails app'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends          'runit'
